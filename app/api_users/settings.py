c6b106b270d6be371072ab22e84869f7d199b1dd
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2025-03-27 12:30:07 -0300

    Habilita regras para forçar senhas seguras

--- app/api_users/settings.py
+++ app/api_users/settings.py
@@ -126,5 +126,23 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 DATE_INPUT_FORMATS = ('%d/%m/%Y')
+
+# Enabling password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 12,
+        },
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
