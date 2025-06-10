7d8c644790182bed30d7ef648052f465c56fafc2
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2025-06-10 11:30:04 -0300

    fix: minor

--- app/profile/models.py
+++ app/profile/models.py
@@ -10,12 +10,12 @@ import requests
 def generate_api_key():
     return secrets.token_hex(16)
 
-def update_api_gateway(profile):
+def update_api_gateway(profile, request):
     url = f"{settings.APISIX_ADMIN_URL}/apisix/admin/consumers"
 
     payload = {
         "username": profile.user.username,
-        "desc": f"LICENCE:{profile.licence_id}. {profile.licence_notes}",
+        "desc": f"{profile.licence_id} / {profile.licence_notes}",
         "plugins": { "key-auth": {
                 "disable": True,
                 "key": profile.api_token
@@ -30,7 +30,7 @@ def update_api_gateway(profile):
     if not response.ok:
         raise Exception(f"API Gateway update failed with status {response.status_code}")
     else:
-        messages.success(profile.user, "O token de API do usuário foi atualizado com sucesso.")
+        messages.success(request, "O token de API do usuário foi atualizado com sucesso.")
 
 # User profile model
 class Profile(models.Model):
@@ -52,6 +52,6 @@ class Profile(models.Model):
         # Call the function to update API Gateway
         if self.licence_id and self.api_token:
             try:
-                update_api_gateway(self)
+                update_api_gateway(self, request)
             except Exception as e:
                 messages.error(request, f"Ocorreu um erro ao tentar atualizar os dados da API. Detalhes do erro: {e}")
