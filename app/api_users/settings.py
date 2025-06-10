df4561e82c1a42fd30577ffff26725b6cb6473d8
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2025-06-10 11:18:58 -0300

    fix: add STATIC_ROOT

--- app/api_users/settings.py
+++ app/api_users/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # Absolute path to the directory static files should be collected to.
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static_files'
 
 STATIC_URL = 'static/'
 
